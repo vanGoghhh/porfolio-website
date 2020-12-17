83a8ec58f1acb34174c241e2e4870af4d402ce3e
vanGoghhh <blackznigelng@gmail.com>
2020-12-17 21:28:30 +0800

    edit AboutMe section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "./AboutMe.css";
 
 const AboutMe = () => {
-  return <div>Bunch of Texts</div>;
+	return (
+		<div className="about-container">
+			<svg viewBox="0 50 500 500" preserveAspectRatio="xMinYMin meet">
+				<path
+					d="M0, 100 C150, 250 350, 
+                0 500, 100 L500, 00 L0, 0 Z"
+					style={{ fill: "#6C28C5", stroke: "none" }}
+				></path>
+			</svg>
+			<div className="about-intro-container">
+				<div className="about-hello">Hello</div>
+				<div className="about-desc">
+					Hi I am Nigel a programming enthusiats whose interests lies <br />
+					in front-end development, reading and the outdoors
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default AboutMe;
