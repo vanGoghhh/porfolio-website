d78758abbc4bb3c464d5984ac0870465e837a9b1
vanGoghhh <blackznigelng@gmail.com>
2020-12-21 22:37:09 +0800

    edit contact component ui

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,24 +11,51 @@ class Contact extends Component {
 		return (
 			<div className="contact-container">
 				<div className="contact-header">Contact</div>
-				<div className="contact-friendly-msg">I'd love to connect with you!</div>
+				<div className="contact-friendly-msg">
+					I'd love to connect with you!
+				</div>
 				<form className="contact-form">
 					<div className="contact-input">
-						<input type="text" name="name"></input>
-						<label>Name</label>
+						<input type="text" className="input-name"></input>
+						<label className="input-name-label">Name</label>
 						<div className="line"></div>
 					</div>
 					<div className="contact-input">
-						<input type="text" name="email"></input>
-						<label>Email</label>
+						<input type="text" className="input-mail"></input>
+						<label className="input-mail-label">Email</label>
 						<div className="line"></div>
 					</div>
 					<div className="contact-input">
-						<textarea type="text" name="message"></textarea>
-						<label>Message</label>
+						<textarea type="text" className="input-message"></textarea>
+						<label className="input-message-label">Message</label>
 						<div className="line"></div>
+					</div>
+					<div className="contact-button-area">
+						<button className="contact-save-button">Save</button>
 					</div>
 				</form>
+				<div className="contact-links">
+					<div className="contact-generic-container">
+						<img className="contact-email-img"></img>
+						<h1>Email Me At</h1>
+						<p>blackznigelng@gmail.com</p>
+					</div>
+					<div className="contact-generic-container">
+						<img className="contact-linkedin-img"></img>
+						<h1>Connect with me on linkedin</h1>
+						<a>Linkedin Profile</a>
+					</div>
+					<div className="contact-generic-container">
+						<img className="contact-phone-img"></img>
+						<h1>Phone me at</h1>
+						<h2>+65 82827731</h2>
+					</div>
+					<div className="contact-generic-container">
+						<img className="contact-github-img"></img>
+						<h1>Find me on github</h1>
+						<a>Github Profile</a>
+					</div>
+				</div>
 			</div>
 		);
 	}
